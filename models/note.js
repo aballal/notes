a30a29b4688dd2cf632d00e5605b743bf6808541
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-16 14:27:34 +0100

    Added unique ID to Note

--- models/note.js
+++ models/note.js
@@ -1,9 +1,20 @@
 (function(exports) {
+  var LAST_USED_ID = -1;
   function Note(text) {
     this._text = text;
+    this._ID = LAST_USED_ID++;
   };
+
   Note.prototype.getText = function() {
     return this._text;
   };
+
+  Note.prototype.getID = function() {
+    return this._ID;
+  };
+
+  //See if you can attach this constant to Note
+  //Note.prototype.LAST_USED_ID = -1;
+
   exports.Note = Note;
 })(this);
--- spec/note-spec.js
+++ spec/note-spec.js
@@ -5,3 +5,15 @@
   };
   testNoteHasAText();
 })(this);
+
+(function(exports) {
+  function testNoteHasAUniqueID() {
+    var note1 = new Note("JavaScript");
+    var note2 = new Note("Ruby");
+    var note3 = new Note("C++");
+    assert.isEqual(note1.getID(),0);
+    assert.isEqual(note2.getID(),1);
+    assert.isEqual(note3.getID(),2);
+  };
+  testNoteHasAUniqueID();
+})(this);
