4fa0c5ba76311aca3f8d039cba0dda681c10f14a
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-17 11:45:49 +0100

    Refactored to export only noteController out to the window

--- controllers/note-controller.js
+++ controllers/note-controller.js
@@ -1,9 +1,18 @@
-var noteList = new NoteList();
+(function(exports){
+  var noteList, noteController;
 
-noteList.createNote("Albert Einstein - We cannot solve our problems with the same thinking we used to create them.");
-noteList.createNote("Richard Feynman - If you can't explain it simply, you don't understand it well enough.");
-noteList.createNote("Batman - It's not who I am underneath but what I do that defines me.");
+  function setup() {
+    noteList = new NoteList();
 
-var noteController = new NoteController(noteList)
-noteController.insertURLIntoApp();
-noteController.makeUrlChangeShowNoteForCurrentPage();
+    noteList.createNote("Albert Einstein - We cannot solve our problems with the same thinking we used to create them.");
+    noteList.createNote("Richard Feynman - If you can't explain it simply, you don't understand it well enough.");
+    noteList.createNote("Batman - It's not who I am underneath but what I do that defines me.");
+
+    noteController = new NoteController(noteList)
+    noteController.insertURLIntoApp();
+    noteController.makeUrlChangeShowNoteForCurrentPage();
+  };
+
+  setup();
+  exports.noteController = noteController;
+})(this);
