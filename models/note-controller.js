9a3960cd857cdbda48b0df44aea64b3c1400c479
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-17 11:09:43 +0100

    Extracted code out of index.html and into NoteController

--- models/note-controller.js
+++ models/note-controller.js
@@ -14,5 +14,24 @@
     document.getElementById('app').innerHTML = this._noteListView.createULOfNotes('url');
   };
 
+  NoteController.prototype.showNote = function(note) {
+    document
+      .getElementById("app")
+      .innerHTML = note;
+  };
+
+  NoteController.prototype.getNoteFromUrl = function(location) {
+    var id = Number(location.hash.split("/")[1]);
+    return this._noteList.getNotes()[id].getText();
+  };
+
+  NoteController.prototype.showNoteForCurrentPage = function() {
+    noteController.showNote(noteController.getNoteFromUrl(window.location));
+  };
+
+  NoteController.prototype.makeUrlChangeShowNoteForCurrentPage = function () {
+    window.addEventListener("hashchange", this.showNoteForCurrentPage);
+  };
+
   exports.NoteController = NoteController;
 })(this);
