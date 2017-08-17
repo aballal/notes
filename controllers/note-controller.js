247ed9c7b4506e0819cc5467eb114b1282d8ca0d
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-17 11:23:29 +0100

    Added missed file in previous commit

---
+++ controllers/note-controller.js
@@ -0,0 +1,9 @@
+var noteList = new NoteList();
+
+noteList.createNote("Albert Einstein - We cannot solve our problems with the same thinking we used to create them.");
+noteList.createNote("Richard Feynman - If you can't explain it simply, you don't understand it well enough.");
+noteList.createNote("Batman - It's not who I am underneath but what I do that defines me.");
+
+var noteController = new NoteController(noteList)
+noteController.insertURLIntoApp();
+noteController.makeUrlChangeShowNoteForCurrentPage();
