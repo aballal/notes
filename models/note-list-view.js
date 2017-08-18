8da53fd45892940816f0dbcddeeb4d0ecb283296
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-18 10:12:24 +0100

    Changed for loop to map, made NoteListView resilient to 'this' changes

--- models/note-list-view.js
+++ models/note-list-view.js
@@ -1,22 +1,19 @@
 (function(exports) {
+  var self;
+
   function NoteListView(noteList) {
-    this._noteList = noteList;
+    self = this;
+    self._noteList = noteList;
   };
 
   NoteListView.prototype.createULOfNotes = function(format) {
     format  = format || 'plain';
-    var notes = this._noteList.getNotes();
 
-    var ulView = "<ul>"
-    for (var i = 0; i < notes.length; i++) {
-      var id = notes[i].getID();
-      var text = notes[i].getText().substr(0,20);
-      if (format == 'url') text = this._createURLOfNote(id, text);
-      ulView += "<li><div id='note-" + id + "'>" + text + "</div></li>";
-    }
-    ulView += "</ul>"
-
-    return ulView;
+    return "<ul>" + self._noteList.getNotes().map( function (note) {
+      var text = note.getText().substr(0,20);
+      if (format === 'url') text = self._createURLOfNote(note.getID(), text);
+      return "<li><div id='note-" + note.getID() + "'>" + text + "</div></li>";
+    }).join("") + "</ul>"
   };
 
   NoteListView.prototype._createURLOfNote = function (id, text) {
