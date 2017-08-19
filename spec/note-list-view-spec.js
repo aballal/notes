7a1dd15863c00643d4c58ee771ccd1207590edb1
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-19 09:44:24 +0100

    Modified tests for NoteListView and aligned it to changes made in the class

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -4,44 +4,24 @@
   function beforeEach() {
     noteList = new NoteList;
     noteListView = new NoteListView(noteList);
-  };
-
-  function testNoteListViewConvertsToHTMLView() {
-    beforeEach();
-    var expectedHTMLView = "<ul></ul>";
-    assert.isEqual(noteListView.createULOfNotes(), expectedHTMLView);
-
-    noteList.createNote("JavaScript");
-    expectedHTMLView = "<ul><li><div>JavaScript</div></li></ul>";
-    assert.isEqual(noteListView.createULOfNotes(), expectedHTMLView);
-
-    noteList.createNote("Ruby");
-    expectedHTMLView = "<ul><li><div>JavaScript</div></li><li><div>Ruby</div></li></ul>";
-    assert.isEqual(noteListView.createULOfNotes(), expectedHTMLView);
-    Note.resetLastUsedId();
-  };
-
-  function testNoteListViewShowsSummary() {
-    beforeEach();
     noteList.createNote("JavaScript is a tricky language");
     noteList.createNote("Ruby is a tidy language");
+  };
 
+  function testNoteListViewCreatesULOfNotesPlain() {
+    beforeEach();
     var expectedHTMLView = "<ul><li><div>JavaScript is a tric</div></li><li><div>Ruby is a tidy langu</div></li></ul>";
     assert.isEqual(noteListView.createULOfNotes(), expectedHTMLView);
     Note.resetLastUsedId();
   };
 
-  function testNoteListViewShowsURLs() {
+  function testNoteListViewCreatesULOfNotesURL() {
     beforeEach();
-    noteList.createNote("JavaScript is a tricky language");
-    noteList.createNote("Ruby is a tidy language");
-
     var expectedHTMLView = "<ul><li><div><a id='note-0' href=\"#notes/0\">JavaScript is a tric</a></div></li><li><div><a id='note-1' href=\"#notes/1\">Ruby is a tidy langu</a></div></li></ul>";
     assert.isEqual(noteListView.createULOfNotes('url'), expectedHTMLView);
     Note.resetLastUsedId();
   };
 
-  testNoteListViewConvertsToHTMLView();
-  testNoteListViewShowsSummary();
-  testNoteListViewShowsURLs();
+  testNoteListViewCreatesULOfNotesPlain();
+  testNoteListViewCreatesULOfNotesURL();
 })(this);
